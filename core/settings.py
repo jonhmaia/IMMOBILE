9ef81469d52a7fcbf562189ba36be25556a6e535
JOAO MARCOS MAIA <joaomarcosmaia@gmail.com>
2024-04-13 18:53:07 -0300

    comunicação com a vercel

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-6+hu2r%xgg4tt75r_c3d_b@ri8#7_3t47#y4b++seht=8$x_r6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
